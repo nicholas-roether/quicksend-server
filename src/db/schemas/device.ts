9bdbc57ca4d9a0a5ac8e980cead0d8c6b4009682
nicholas-roether <nicholas.roether@t-online.de>
2022-04-26 05:34:41 +0200

    swap reference direction between user and device models

--- src/db/schemas/device.ts
+++ src/db/schemas/device.ts
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
+import UserModel from "../models/user";
 
 interface Device {
 	name: string;
+	user: mongoose.Types.ObjectId;
 	signaturePublicKey: string;
 }
 
 const DeviceSchema = new mongoose.Schema<Device>({
 	name: { type: String, required: true },
+	user: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: UserModel },
 	signaturePublicKey: { type: String, required: true }
 });
 
--- src/db/schemas/user.ts
+++ src/db/schemas/user.ts
@@ -5,18 +5,12 @@ interface User {
 	username: string;
 	display?: string;
 	passwordHash: string;
-	devices: mongoose.Types.ObjectId[];
 }
 
 const UserSchema = new mongoose.Schema<User>({
 	username: { type: String, required: true, unique: true, index: true },
 	display: String,
-	passwordHash: { type: String, required: true },
-	devices: {
-		type: [mongoose.SchemaTypes.ObjectId],
-		default: [],
-		ref: DeviceModel
-	}
+	passwordHash: { type: String, required: true }
 });
 
 export default UserSchema;
