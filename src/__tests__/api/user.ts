dd94b3320bf1640b6f884caf242072149ad81c51
nicholas-roether <nicholas.roether@t-online.de>
2022-04-28 16:40:22 +0200

    add more api tests

--- src/__tests__/api/user.ts
+++ src/__tests__/api/user.ts
@@ -39,5 +39,24 @@ describe("POST /user/create ", () => {
 		expect(user.username).toBe("test-user");
 		expect(user.display).toBe("Test User");
 		expect(bcrypt.compareSync("password1234", user.passwordHash)).toBe(true);
+
+		expect(response.body).toEqual({ data: { id: user._id.toHexString() } });
+	});
+
+	test("Handles duplicate users", async () => {
+		const user = new UserModel({
+			username: "test_user_2",
+			passwordHash: "gsredtzdhrfgjf"
+		});
+		await user.save();
+
+		const response = await request(app)
+			.post("/user/create")
+			.send({ username: "test_user_2", password: "fgsdrtghfthjgz" })
+			.expect(400);
+
+		expect(response.body).toHaveProperty("error");
+		const users = await UserModel.find().exec();
+		expect(users.length).toBe(1);
 	});
 });
