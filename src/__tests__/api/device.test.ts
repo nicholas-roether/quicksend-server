ae5473598cf36e2bbd8e7fbf6b3db0923ca4bf33
nicholas-roether <nicholas.roether@t-online.de>
2022-04-29 23:19:11 +0200

    Add more tests to POST /device/add

--- src/__tests__/api/device.test.ts
+++ src/__tests__/api/device.test.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import { expect } from "chai";
 import DeviceModel from "src/db/models/device";
 import UserModel from "src/db/models/user";
+import { User } from "src/db/schemas/user";
 import supertest from "supertest";
 import { createMongooseConnection } from "../__utils__/mongoose";
 import { createTestServer } from "../__utils__/server";
@@ -19,13 +20,48 @@ describe("POST /device/add", () => {
 		username: "test-user",
 		password: "password123"
 	};
+	let testUser: User;
 
 	beforeEach(async () => {
-		const testUser = new UserModel({
+		const testUserDoc = new UserModel({
 			username: user.username,
 			passwordHash: bcrypt.hashSync(user.password, 10)
 		});
-		await testUser.save();
+		await testUserDoc.save();
+		testUser = testUserDoc;
+	});
+
+	it("should require a value for name", async () => {
+		await request
+			.post("/device/add")
+			.send({
+				signaturePublicKey: "sfaggfdhs",
+				encryptionPublicKey: "sghdffdghjhfgdj"
+			})
+			.auth(user.username, user.password, { type: "basic" })
+			.expect(400);
+	});
+
+	it("should require a value for signaturePublicKey", async () => {
+		await request
+			.post("/device/add")
+			.send({
+				name: "sfaggfdhs",
+				encryptionPublicKey: "sghdffdghjhfgdj"
+			})
+			.auth(user.username, user.password, { type: "basic" })
+			.expect(400);
+	});
+
+	it("should require a value for encryptionPublicKey", async () => {
+		await request
+			.post("/device/add")
+			.send({
+				name: "sdfdsgfh",
+				signaturePublicKey: "sfaggfdhs"
+			})
+			.auth(user.username, user.password, { type: "basic" })
+			.expect(400);
 	});
 
 	context("device with name does not exist", () => {
@@ -51,6 +87,62 @@ describe("POST /device/add", () => {
 			expect(res.body).to.deep.equal({
 				data: { id: device._id.toHexString() }
 			});
+		});
+
+		it("should add device with name, type and public keys", async () => {
+			const res = await request
+				.post("/device/add")
+				.send({
+					name: "Test Device",
+					type: 2,
+					signaturePublicKey: "sfdghhgjkkzlg",
+					encryptionPublicKey: "sdfghhjjghfk"
+				})
+				.auth(user.username, user.password, { type: "basic" })
+				.expect(201);
+
+			const devices = await DeviceModel.find().exec();
+			expect(devices.length).to.equal(1);
+			const device = devices[0];
+			expect(device.name).to.equal("Test Device");
+			expect(device.type).to.equal(2);
+			expect(device.signaturePublicKey).to.equal("sfdghhgjkkzlg");
+			expect(device.encryptionPublicKey).to.equal("sdfghhjjghfk");
+
+			expect(res.body).to.deep.equal({
+				data: { id: device._id.toHexString() }
+			});
+		});
+	});
+
+	context("device with name already exists", () => {
+		beforeEach(async () => {
+			const device = new DeviceModel({
+				name: "Test Device",
+				user: testUser._id,
+				signaturePublicKey: "restghjgdf",
+				encryptionPublicKey: "tshghjghjf"
+			});
+			await device.save();
+		});
+		it("should return a 400 response and not add a device", async () => {
+			await request
+				.post("/device/add")
+				.send({
+					name: "Test Device",
+					signaturePublicKey: "sfdghhgjkkzlg",
+					encryptionPublicKey: "sdfghhjjghfk"
+				})
+				.auth(user.username, user.password, { type: "basic" })
+				.expect(400);
+
+			const devices = await DeviceModel.find().exec();
+			expect(devices.length).to.equal(1);
+			const device = devices[0];
+			expect(device.name).to.equal("Test Device");
+			expect(device.type).to.equal(0);
+			expect(device.signaturePublicKey).not.to.equal("sfdghhgjkkzlg");
+			expect(device.encryptionPublicKey).not.to.equal("sdfghhjjghfk");
 		});
 	});
 });
